eaacd6ee82b6787fd4ceb1314ec58d09f60969a7
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-03-18 14:44:19 +0100

    PC Testbench complete

--- tests/pc_tb.vhd
+++ tests/pc_tb.vhd
@@ -13,6 +13,7 @@ use IEEE.std_logic_1164.all;
 
 library work;
 use work.sdvu_constants.all;
+use work.tb_helpers.all;
 
 -- =================
 --      Entity
@@ -29,15 +30,15 @@ architecture arch_pc_tb of pc_tb is
     -- Internal Objects
     -- Clock, Reset and Enable signals
     constant HALF_PERIOD : time := 5 ns; -- Clock half period
-    signal clock     : std_logic  := '0';  -- Clock signal
+    signal clock   : std_logic  := '0';  -- Clock signal
+    signal enable  : std_logic  := '0';  -- Enable signal
     signal reset   : std_logic  := '0';  -- Reset signal
     signal running : boolean    := true; -- Running flag, Simulation continues while true
 
      -- Entity Signals
-     signal I_PC        : STD_LOGIC_VECTOR (PC_SIZE-1 downto 0);
-     signal I_PC_OPCode : STD_LOGIC_VECTOR (1 downto 0);
+     signal I_newPC     : STD_LOGIC_VECTOR (PC_SIZE-1 downto 0);
+     signal I_PC_OPCode : STD_LOGIC_VECTOR (PC_OP_SIZE-1 downto 0);
      signal O_PC        : STD_LOGIC_VECTOR (PC_SIZE-1 downto 0);
-
 
 begin
     -- Clock, Reset and Enable generation
@@ -58,53 +59,44 @@ begin
 
     -- DUT
     dut: entity work.pc(arch_pc)
-        generic map (
-          PC_SIZE => PC_SIZE
-        )
-        port map (
-          I_clock       => clock,
-          I_reset     => reset,
-          I_PC        => I_PC,
-          I_PC_OPCode => I_PC_OPCode,
-          O_PC        => O_PC
-        );
+      port map (
+        I_clock     => clock,
+        I_reset     => reset,
+        I_enable    => enable,
+        I_newPC     => I_newPC,
+        I_PC_OPCode => I_PC_OPCode,
+        O_PC        => O_PC
+      );
 
     -- Stimulus process
     StimulusProcess: process
     begin
       wait until reset = '0';
-      wait_cycles(10);
+      wait until enable = '1';
       report "PC: Running testbench";
       -- TESTING OPERATIONS
 
       -- Test 1: ASSIGN | Assign PC
       I_PC_OPCode <= PC_OP_ASSIGN;
-      I_PC <= X"FEED";
-      wait_cycles(2);
-      if (O_PC=X"FEED") then report "Test ASSIGN: Passed" severity NOTE;
-        else report "Test ASSIGN: Failed" severity FAILURE;
-      end if;
+      I_newPC<= X"FEED";
+      wait_cycles(clock, 2);
+      assert_true(O_PC=X"FEED", "Assign");
 
       -- Test 2: NOP | Do nothing
       I_PC_OPCode <= PC_OP_NOP;
-      wait_cycles(2);
-      if (O_PC=X"FEED") then report "Test NOP: Passed" severity NOTE;
-        else report "Test NOP: Failed" severity FAILURE;
-      end if;
+      wait_cycles(clock, 1);
+      assert_true(O_PC=X"FEED", "No operation");
 
       -- Test 3: INC | Increment the PC
       I_PC_OPCode <= PC_OP_INC;
-      wait_cycles(2);
-      if (O_PC=X"FEEE") then report "Test INC: Passed" severity NOTE;
-        else report "Test INC: Failed" severity FAILURE;
-      end if;
+      wait_cycles(clock, 1);
+      assert_true(O_PC=X"FEEE", "Increment");
+
 
       -- Test 4: RESET | Reset the PC
       I_PC_OPCode <= PC_OP_RESET;
-      wait_cycles(2);
-      if (O_PC=X"0000") then report "Test RESET: Passed" severity NOTE;
-        else report "Test RESET: Failed" severity FAILURE;
-      end if;
+      wait_cycles(clock, 1);
+      assert_true(O_PC=X"0000", "Reset");
 
       running <= false;
       report "PC: Testbench complete";
